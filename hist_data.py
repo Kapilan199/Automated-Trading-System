8419d14a3c0606011590071544c920e827e8df52
Kapilan Ramasamy <kapilanr2003@gmail.com>
2023-12-21 17:18:09 -0800

    collection of historical data

--- hist_data.py
+++ hist_data.py
@@ -5,6 +5,9 @@ mt5 historical data collection
 """
 
 import MetaTrader5 as mt5
+import datetime as dt
+import pandas as pd
+import pytz
 
 # need a .txt file with login,pass,server
 # need to connect to mt5 terminal also need terminal64.exe 
@@ -27,7 +30,7 @@ except IOError:
 
 # establish MetaTrader 5 connection to a specified trading account
     
-# add scheduler to further automate the system
+# NOTE: add scheduler to further automate the system
 
 try:
     connection = mt5.initialize(path=path, login=int(key[0]), server=key[2], password=key[1])
@@ -36,3 +39,28 @@ try:
 except mt5.Error as error:
     print(f"initialize() failed, error code = {error.code}")
     quit()
+
+
+
+
+
+# Now we must extract historical data
+    
+ # set time zone to LA
+timezone = pytz.timezone("America/Los_Angeles")
+
+# get 200 EURUSD H4 bars starting from 10/1/202 - M/D/Y in Los Angeles time zone
+
+hist_data = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_M15, dt.datetime(2023, 10, 1,tzinfo=timezone), 200)
+
+hist_data_df = pd.DataFrame(hist_data)
+
+hist_data_df_time = pd.to_datetime(hist_data_df.time, unit="s")
+
+hist_data_df.time = hist_data_df_time
+
+# inplace means to permanently make the index time
+
+hist_data_df.set_index("time", inplace=True)
+
+print(hist_data_df)
